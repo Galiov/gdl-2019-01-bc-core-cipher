db2db0289df52645c70309d23e9789aaed0a08e8
Galilea <galilea.valle@gmail.com>
2019-01-29 12:01:45 -0600

    Hice pruebas

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,32 @@
 window.cipher = {
   // ... 
 };
+
+
+const x = 4;
+const y = 5;
+
+function add(x,y){
+    let resultado = x + y;
+    return resultado;
+}
+
+console.log(add(3,3));
+
+function multiply(num1, num2){
+	return num1*num2
+}
+
+const menu1 = ['Pozole', 'Tacos al Pastor', 'Ceviche', 'Pizza']; //Ceviche
+const menu2 = ['Birria', 'Enchiladas', 'Tacos al Pastor']; //Tacos
+const menu3 = ['Empanada Chilena', 'Quesadilla']; //Empanada
+const menu4 = ['Sushi','Hamburguesa', 'Waffles', 'Torta']; //Hamburguesa
+const menu5 = ['Spaggetti', 'Sopes']; //
+
+const orden = (menu) => {
+    for(let i=0; i<menu.length-1;i++){
+    	console.log(menu[i]);
+
+    }
+}
+orden(menu5)
