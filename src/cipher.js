c4b4748a81ad44c38b0141d2a415279c01498566
Galilea <galilea.valle@gmail.com>
2019-02-06 08:18:24 -0600

    Los Test pasan correctamente

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,47 @@
-window.cipher = {}
+window.cipher = {
+  
+encode: (offset, string)=> {
+//Declaro un string nuevo vacio donde voy a guardar mi mensaje encriptado
+let newString = '';
+let i;
+ //Ciclo for para encriptar letra por letra
+ for(i = 0; i < string.length; i++){
 
-	let offset = document.getElementById("number");
-	let string = document.getElementById("input");
-	 //Funcion para codificar el mensaje
-	 function encode(offset, string){
-
-	  //Declaro un string nuevo vacio donde voy a guardar mi mensaje encriptado
-	  let newString = '';
-	  let i;
-	  //Ciclo for para encriptar letra por letra
-	  for(i = 0; i < string.length; i++){
-
-		 //Guardo el caracter en la posicion i en una variable
-		let x = string.charCodeAt(i);
+//Guardo el caracter en la posicion i en una variable
+	let x = string.charCodeAt(i);
 
 		//Si el valor de desplazamiento es mayor a 26, agarro el residuo
 		//para que empiece nuevamente el abecedario
-		 if(offset > 26){
-		 offset = offset % 26;
-		 }
+	if(offset > 26){
+		offset = offset % 26;
+	}
 
 		//Defino el rango de las letras del abecedario, dentro de ASCII
-		if ((x >= 65) && (x <= 90)){
+	if ((x >= 65) && (x <= 90)){
 
 		//Aplico la formula de cifrado y guardo el caracter nuevo en una variable
-		let y = string.fromCharCode(((x - 65 + offset)%26) + 65);
+	let y = String.fromCharCode(((x - 65 + offset)%26) + 65);
 		//Actualizo mi nuevo string, concatenando las letras cifradas
-		newString = newString + y
-	   }
-	  }
-	  let output = newString;
-	  return output.innerHTML();
-    
-	};
+	newString = newString + y;
+	}
+	}
+	
+	return newString;
+	},
+
+decode: (offset, string) => {
+let newString = '';
+let i;
+	for(i = 0; i < string.length; i++){
+		let x = string.charCodeAt(i);
+		if (offset > 26) {
+			offset = offset % 26;
+		}
+		if((x >= 65) && (x <= 90)){
+		let y = String.fromCharCode(((x + 65 - offset)%26) +65);
+		newString = newString + y;
+		}	
+		}
+	return newString;
+	},
+};
--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 
-let divElement = document.getElementById(screen1);
-let divElement2 = document.getElementById(screen2);
-let divElement3 = document.getElementById(screen3);
 
-function toggleVisibility(divElement2,divElement,divElement3){
- //Ocultando la division del inicio
-if(divElement.style.display == 'none'){
-   divElement.style.display = 'block';
- }
- else {
-   divElement.style.display = 'none';
- }
+function getValues (){
+ let offset = document.getElementById("offsetNumber").value;
+ let string = document.getElementById("input").value;
+ let result;
 
- //Mostrando la division del menu
-if(divElement2.style.display == 'none'){
-   divElement2.style.display = 'block';
- }
- else {
-   divElement2.style.display = 'none';
- }
-
-if(divElement3.style.display == 'none'){
-   divElement3.style.display = 'block';
- }
- else {
-   divElement3.style.display = 'none';
- }
+ result = cipher.encode(offset, string);
+ displayResult(result);
 }
 
+function displayResult (result){
+  document.getElementById("result").value = result;
+}
+
+document.getElementById("cifrar").addEventListener("click", getValues);
+document.getElementById("decifrar").addEventListener("click", getValues);
